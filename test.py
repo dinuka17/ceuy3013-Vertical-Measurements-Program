6e0459ac15737b1f65a8b9a8bd1c9b50a5082682
dinuka17 <69228228+dinuka17@users.noreply.github.com>
2020-12-13 21:27:39 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,13 @@
 # of importing a class from "source.py", instantiating a new object and printing
 # that object. Replace the code below with your own.
 
-from source import TestClass
+from source import Adequacy
 
-t = TestClass(5)
-
-if __name__ == '__main__':
-    print(t)
+print("What are your points being used for?")
+print("")
+print("1. Monument or Survey")
+print("2. Engineering or Research Project")
+print("3. Topography Map or Drainage Map")
+print("")
+user_input = input("Pick 1, 2, or 3: ")
+aduequacy = Adequacy('Vertical Measurements.csv', user_input)
