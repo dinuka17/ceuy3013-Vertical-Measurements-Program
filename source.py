18d63e92fdea86aed82e3ccd671322450ace8ac6
dinuka17 <69228228+dinuka17@users.noreply.github.com>
2020-12-21 22:02:07 -0500

    Update source.py

--- source.py
+++ source.py
@@ -3,8 +3,6 @@
 # that can be used to build larger processes. For example, the class you
 # created for the OOP assignment would go here.
 
-# Here is a test class, replace the code below with your own
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,15 +21,27 @@ class Adequacy:
     
 
   def find_error_of_closure(self, df):
-    row_difference = df["H.I."]-df["F.S. (-)"]
-    df['Elevation'] = row_difference
-    df.to_csv('New Initial Meaurements.csv')
-    df['Cumulative Length of Course(FEET)'] = df['LENGTH COURSE (FEET)'].cumsum()
-    total_course_length = df.at[4,'Cumulative Length of Course(FEET)']
+    row_difference = df["H.I."]-df["F.S. (-)"] #Adding additional Elevation column to initial data
+    df['Elevation'] = row_difference 
+    df.to_csv('New Initial Meaurements.csv') #Now we will save this new updated initial dataset
+#The next step is to find the cumulative length when going from one station to the next. 
+#The end goal is to find if the Allowable Error of Closure for whatever Order this program is for, is good or not. 
+#If the Error of Closure is less than the Allowable Error of Closure for Whatever order and whatever length the program is intended for, then those points can be used to construct a site layout. 
+    df['Cumulative Length of Course(FEET)'] = df['LENGTH COURSE (FEET)'].cumsum() #The goal of this cell block is to create an entirely new column that has the cumulative sum of the Course Length Column
+# With the cumulative length column, the next step is finding the ratio of the cumulative length over the total length of the course. 
+#The total length of the entire course is the last value in the Cumulative Length fo course (FEET) column. 
+#Therefore, we will need to create a new variable that takes this point from our CSV
+    total_course_length = df.at[4,'Cumulative Length of Course(FEET)'] # applying at/iat function to retrieve a value from a Pandas 
+#First Class: Used to establish permanent survey monuments for National Control Network
+#Second Class: Used for major Engineering and Research Projects
+#Third Class: Serves as vertical references for projects, topography maps and drainage maps
     self.first_order_equation = 0.12*((total_course_length/3281)**0.5)
     self.second_order_equation = 0.24*((total_course_length/3281)**0.5)
     self.third_order_equation = 0.48*((total_course_length/3281)**0.5)
     df['Length Ratio'] = df['Cumulative Length of Course(FEET)'] / total_course_length
+#The next step is adding a correction column to our DataFrame. To obtain the correction factor, the length ratio of each point must
+#be multiplied by the difference in the elevation of the First Point, and the last. The first and last points are the same, but due
+#to human error in taking measurements due to differential leveling, they are not the same. 
     first_and_last_points_difference = df.at[0,'Elevation'] - df.at[4,'Elevation']
     df['Correction Factor'] = df['Length Ratio'] * first_and_last_points_difference
     df['Adjusted Elevations'] = df['Elevation'] - df['Correction Factor']
@@ -79,5 +89,4 @@ def main():
   aduequacy = Adequacy('Vertical Measurements.csv', user_input)
 
 main()
-
 
